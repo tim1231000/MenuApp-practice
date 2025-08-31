d5054b55175ef042c1d52cb883000e2e1799cd65
2u2okREal <68776433+tim1231000@users.noreply.github.com>
2025-09-01 07:54:25 +0900

    adding MenuListRow struct to simplify the MenuView

---
+++ Menu/MenuListRow.swift
@@ -0,0 +1,18 @@
+//
+//  MenuListRow.swift
+//  Menu
+//
+//  Created by 최욱진 on 9/1/25.
+//
+
+import SwiftUI
+
+struct MenuListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuListRow()
+}
